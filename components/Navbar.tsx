2b17b28400ff2dd9263f93388b07687aa46cebb6
Waldid32 <waldid1817@gmail.com>
2025-01-24 11:47:24 -0500

    style: cambio gradiante

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-primaryGradiante fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+    <nav className="bg-bgPrimaryGradiante fixed w-full z-20 top-0 start-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
         <span className="self-center text-2xl font-semibold whitespace-nowrap border-2 border-segundary p-2 rounded-lg">

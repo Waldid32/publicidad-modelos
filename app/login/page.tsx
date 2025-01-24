e61e49316f27ba04d32daba9f5460f04590a6c2b
Waldid32 <waldid1817@gmail.com>
2025-01-23 19:51:06 -0500

    style: Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function Login() {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Inicia sesión en tu cuenta
@@ -99,6 +99,13 @@ export default function Login() {
                 disabled={isPending}
               >
                 {isPending ? "Cargando..." : "Iniciar Sesión"}
+              </button>
+              <button
+                type="button"
+                className="w-full text-white bg-primary-900 hover:bg-primary-950 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={() => router.push("/")}
+              >
+                Rregresar
               </button>
             </form>
           </div>

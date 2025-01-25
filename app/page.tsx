77c7109be2c996f46bfe87d1c8129418456c6359
Waldid32 <waldid1817@gmail.com>
2025-01-25 11:40:28 -0500

    feat: Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { useState } from "react";
+
+import { BoxModel } from "@/components/BoxModel";
+import { Navbar } from "@/components/Navbar";
+import { SearchModels } from "@/components/SearchModels";
+
 export default function Home() {
+  const [dataModels, setDataModels] = useState([]);
+
+  console.log(dataModels);
+
   return (
     <div>
-      <h1 className="text-5xl">HOLA MUNDO</h1>
+      <Navbar />
+      <div className="pt-32 pb-5 border-b border-gray-200 dark:border-gray-600 bg-primary">
+        <SearchModels setDataModels={setDataModels} />
+      </div>
+
+      <div>
+        <BoxModel dataModels={dataModels} />
+      </div>
     </div>
   );
 }

b47e2ba4820314e793d3620334a0696d2445b297
Waldid32 <waldid1817@gmail.com>
2025-01-25 17:46:29 -0500

    delete: flowbite-react

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/**/*.js", // Agrega Flowbite aquí
   ],
   theme: {
     extend: {
@@ -19,7 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("flowbite/plugin")], // Agrega el plugin de Flowbite
 };
 
 export default config;

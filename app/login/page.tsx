55ed0679bbd3e7a362a8920b3dec4471e9e645e8
Waldid32 <waldid1817@gmail.com>
2025-01-25 17:42:26 -0500

    style: minusculas el nombre de usuario

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -50,7 +50,7 @@ export default function Login() {
                   id="nombreUsuario"
                   value={nombreUsuario}
                   onChange={(e) => setNombreUsuario(e.target.value)}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 "
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 lowercase"
                   placeholder="Nombre de usuario"
                   required
                 />

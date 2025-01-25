b3843a875f31b44037765d221c99e07e9bb245e1
Waldid32 <waldid1817@gmail.com>
2025-01-25 17:43:21 -0500

    feat: layout Modelos

---
+++ app/modelo/layout.tsx
@@ -0,0 +1,26 @@
+import NavBarLogin from "@/components/NavBarLogin";
+import { cookies } from "next/headers";
+
+export const metadata = {
+  title: "Sección Modelos",
+};
+
+export default async function ModeloLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Este componente es un Server Component por defecto en Next 13/14
+  const cookieStore = await cookies();
+
+  const role = cookieStore.get("role")?.value || "";
+  const nombreUsuario = cookieStore.get("nombreUsuario")?.value || "";
+  const nombreCompleto = cookieStore.get("nombreCompleto")?.value || "";
+
+  return (
+    <>
+      <NavBarLogin role={role} nombreCompleto={nombreCompleto} />
+      <main className="pt-10">{children}</main>
+    </>
+  );
+}

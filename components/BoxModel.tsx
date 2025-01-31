f36e9765a3b9d1fdcbe1afb58ea7078709f4e3ba
Waldid32 <waldid1817@gmail.com>
2025-01-31 00:31:53 -0500

    refactor: eslint

--- components/BoxModel.tsx
+++ components/BoxModel.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 
-export function BoxModel({ dataModels }: any) {
+interface PropsBoxModel {
+  dataModels: {
+    nombreCompleto: string;
+    edad: string;
+    descripcion: string;
+    etnia: string;
+    zona: string;
+  };
+}
+
+export function BoxModel({ dataModels }: PropsBoxModel) {
   // Verificar que sea array antes de mapear
   const modelsArray = Array.isArray(dataModels) ? dataModels : [];
 
@@ -9,7 +19,7 @@ export function BoxModel({ dataModels }: any) {
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
           {modelsArray.length > 0 ? (
-            modelsArray.map((data: any, index: number) => (
+            modelsArray.map((data: PropsBoxModel, index: number) => (
               <div
                 className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700"
                 key={index}
@@ -25,19 +35,20 @@ export function BoxModel({ dataModels }: any) {
                 </a>
                 <div className="p-5">
                   <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    <a href="#">{data.nombreCompleto}</a>
+                    <a href="#">{data.dataModels.nombreCompleto}</a>
                   </h3>
                   <span className="text-gray-500 dark:text-gray-400">
-                    <strong>Edad:</strong> {data.edad}
+                    <strong>Edad:</strong> {data.dataModels.edad}
                   </span>
                   <span className="text-gray-500 dark:text-gray-400">
-                    {data.etnia}
+                    {data.dataModels.etnia}
                   </span>
                   <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
-                    {data.descripcion ?? "Descripción no disponible."}
+                    {data.dataModels.descripcion ??
+                      "Descripción no disponible."}
                   </p>
                   <span className="text-gray-500 dark:text-gray-400">
-                    {data.zona}
+                    {data.dataModels.zona}
                   </span>
                 </div>
               </div>

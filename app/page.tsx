ac8b74b2d76352c9ab383b5f5a0443b7531b67b9
Waldid32 <waldid1817@gmail.com>
2025-02-03 17:36:02 -0500

    fix: Eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-"use client";
-import { useState } from "react";
-
 import { Navbar } from "@/components/Navbar";
-import { ModelData } from "@/types/types";
 import { TopModels } from "@/components/TopModels";
 
 export default function Home() {
-  const [dataModels, setDataModels] = useState<ModelData[]>([]);
-
   return (
     <div>
       <Navbar />
-      <div></div>
-      <TopModels />
+      <div>
+        <TopModels />
+      </div>
     </div>
   );
 }

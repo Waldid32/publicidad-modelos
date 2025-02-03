d1a6d1461dc0769552ff53de58ef5e3cc3c55b99
Waldid32 <waldid1817@gmail.com>
2025-02-03 16:59:53 -0500

    feat: GET models

---
+++ app/api/models/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    // Aquí llamas a tu API externa u otro microservicio
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/users/models`
+    );
+    if (!response.ok) {
+      throw new Error(
+        "Error en la consulta de las modelos, intente mas tarde!"
+      );
+    }
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error en la consulta de las modelos, intente mas tarde!" },
+      { status: 500 }
+    );
+  }
+}

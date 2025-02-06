fe8cc74daab99fd0bdcc4b7dd29b0c83f83fcdba
Waldid32 <waldid1817@gmail.com>
2025-02-06 17:12:58 -0500

    fix: bug uri get models

--- app/api/models/route.ts
+++ app/api/models/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     // Aquí llamas a tu API externa u otro microservicio
-    const response = await fetch(`${process.env.API_URL}/users/models`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/models`);
     if (!response.ok) {
       throw new Error(
         "Error en la consulta de las modelos, intente mas tarde!"
@@ -12,7 +12,8 @@ export async function GET() {
     const data = await response.json();
 
     return NextResponse.json(data);
-  } catch {
+  } catch (error) {
+    console.log(error)
     return NextResponse.json(
       { message: "Error en la consulta de las modelos, intente mas tarde!" },
       { status: 500 }

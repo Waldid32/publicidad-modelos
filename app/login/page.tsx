8b10897124db782fab4489bf1300a2e422f84595
Waldid32 <waldid1817@gmail.com>
2025-02-06 09:57:35 -0500

    fix: bug en nombre de usuario

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -49,10 +49,13 @@ export default function Login() {
                   name="nombreUsuario"
                   id="nombreUsuario"
                   value={nombreUsuario}
-                  onChange={(e) => setNombreUsuario(e.target.value)}
+                  onChange={(e) =>
+                    setNombreUsuario(e.target.value.toLowerCase())
+                  }
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 lowercase"
                   placeholder="Nombre de usuario"
                   required
+                  autoCapitalize="none"
                 />
               </div>
               <div>
@@ -68,6 +71,8 @@ export default function Login() {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 "
                   required
+                  autoCapitalize="none"
+                  autoCorrect="off"
                 />
               </div>
               <div className="flex items-center justify-between gap-5">
--- app/registerCliente/page.tsx
+++ app/registerCliente/page.tsx
@@ -130,6 +130,7 @@ export default function RegisterCliente() {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 lowercase"
                   required
+                  autoCapitalize="none"
                 />
               </div>
 

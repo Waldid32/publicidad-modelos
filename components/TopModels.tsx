16207de0da36fefc8e218b13e272748d6c4aaad0
Waldid32 <waldid1817@gmail.com>
2025-02-08 01:13:24 -0500

    refactor: TopModels

--- components/TopModels.tsx
+++ components/TopModels.tsx
@@ -1,54 +1,13 @@
 "use client";
 
-import { ModelData } from "@/types/types";
-import { useEffect, useState } from "react";
-import { toast } from "sonner";
-import { BoxModel } from "./BoxModel";
+import { useFetchModels } from "@/hooks/useFetchModels";
+import { ModelList } from "./ModelList";
 
 export function TopModels() {
-  const [models, setModels] = useState<ModelData[]>([]);
+  const { models, loading, error } = useFetchModels("/api/models");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/models");
+  if (loading) return <p className="text-center text-lg">Cargando modelos...</p>;
+  if (error) return <p className="text-center text-lg text-red-500">{error}</p>;
 
-        if (!response.ok) throw new Error("Error al consultar");
-
-        const rawData = await response.json();
-
-        const transformedData: ModelData[] = rawData.map((item: ModelData) => {
-          return {
-            nombreCompleto: item.nombreCompleto,
-            edad: item.edad,
-            descripcion: item.descripcion,
-            etnia: item.etnia,
-            zona: item.zona,
-            multimedias: item.multimedias,
-            numeroContacto: item.numeroContacto,
-          };
-        });
-
-        setModels(transformedData);
-      } catch (error) {
-        if (error instanceof Error) {
-          toast.error(error.message);
-        } else {
-          toast.error("Error desconocido");
-        }
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h1 className="font-bold text-2xl text-center py-5 bg-segundary text-white">
-        Top Models
-      </h1>
-
-      <BoxModel dataModels={models} />
-    </div>
-  );
+  return <ModelList dataModels={models} />;
 }

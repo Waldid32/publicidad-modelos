099f194d87faaeebe1949e12f191f4b6bdb6b2b9
Waldid32 <waldid1817@gmail.com>
2025-02-09 15:35:18 -0500

    feat: route get token

---
+++ app/api/auth/token/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+
+export async function GET() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("access_token")?.value;
+    const jwtSecret = process.env.JWT_SECRET!;
+
+    if (!token) {
+        return NextResponse.json({ error: "No autenticado" }, { status: 401 });
+    }
+
+    if (!jwtSecret) {
+        return NextResponse.json({ error: "Error de configuración del servidor" }, { status: 500 });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        return NextResponse.json({
+            token,
+            decoded
+        }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: "Token inválido" }, { status: 401 });
+    }
+}

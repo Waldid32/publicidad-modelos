67574fe6bbf25694b94eda63479b7c470e7c66b2
Waldid32 <waldid1817@gmail.com>
2025-02-09 15:35:05 -0500

    refactor: mejora de top models

--- components/TopModels.tsx
+++ components/TopModels.tsx
@@ -2,12 +2,27 @@
 
 import { useFetchModels } from "@/hooks/useFetchModels";
 import { ModelList } from "./ModelList";
+import { ModelData } from "@/types/types";
 
-export function TopModels() {
+interface TopModelsProps {
+  favoritos?: ModelData[];
+  toggleFavorito?: (modelo: ModelData) => void;
+  isLoggedIn?: boolean;
+}
+
+
+export function TopModels({ favoritos, toggleFavorito, isLoggedIn }: TopModelsProps) {
   const { models, loading, error } = useFetchModels("/api/models");
 
   if (loading) return <p className="text-center text-lg">Cargando modelos...</p>;
   if (error) return <p className="text-center text-lg text-red-500">{error}</p>;
 
-  return <ModelList dataModels={models} />;
+  return (
+    <ModelList
+      dataModels={models}
+      favoritos={favoritos}
+      toggleFavorito={toggleFavorito}
+      isLoggedIn={isLoggedIn}
+    />
+  );
 }

8ebed47b64c943e3c67f0fcd45dee3c2cf7e8fbe
Waldid32 <waldid1817@gmail.com>
2025-02-17 14:44:54 -0500

    refactor: update cookies

---
+++ app/api/auth/update-suscription/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+    // Aquí procesas la compra y actualizas la base de datos
+    const updatedUserData = {
+        suscripcionBasica: false,
+        suscripcionPremiun: true, // Supongamos que se compró la premium
+    };
+
+    // Actualiza las cookies con los nuevos valores
+    const cookieStore = await cookies();
+    cookieStore.set("suscripcionBasica", String(updatedUserData.suscripcionBasica), {
+        httpOnly: true,
+        path: "/",
+    });
+    cookieStore.set("suscripcionPremiun", String(updatedUserData.suscripcionPremiun), {
+        httpOnly: true,
+        path: "/",
+    });
+
+    return NextResponse.json({ success: true });
+}
